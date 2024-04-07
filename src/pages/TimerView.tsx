ab384f48f0c68795c5966caf54bd86f72c82b83a
Atsuya Uchida <atsuya_eagle@outlook.com>
2024-04-08 01:14:51 +0900

    Fix timer display and update interval

--- src/pages/TimerView.tsx
+++ src/pages/TimerView.tsx
@@ -27,15 +27,14 @@ export function TimerView(props: TimerViewProps) {
     if (remainingSeconds <= 0) {
       return "00:00";
     }
-    const floorSeconds = Math.floor(remainingSeconds);
-    const min = Math.floor(floorSeconds / 60);
-    const sec = floorSeconds % 60;
+    const min = remainingSeconds / 60
+    const sec = remainingSeconds % 60;
     return `${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec}`;
   }
 
   useEffect(() => {
     const id = setInterval(() => {
-      if (elapsedSeconds + 0.1 >= timerToSeconds(props.timers[currentTimerIdx])) {
+      if (elapsedSeconds + 1 >= timerToSeconds(props.timers[currentTimerIdx])) {
         if (currentTimerIdx === props.timers.length - 1) {
           clearInterval(id);
           return;
@@ -43,8 +42,8 @@ export function TimerView(props: TimerViewProps) {
         setCurrentTimerIdx(currentTimerIdx => currentTimerIdx + 1);
         setElapsedSeconds(0);
       }
-      setElapsedSeconds(totalSecs => totalSecs + 0.1);
-    }, 100);
+      setElapsedSeconds(totalSecs => totalSecs + 1);
+    }, 1000);
     return () => clearInterval(id);
   })
 
